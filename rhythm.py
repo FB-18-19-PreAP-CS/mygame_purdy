a3fcb2b80a45e4f1273d30afc6c636e7d289094a
Chad Purdy <cpurdy@flourbluffschools.net>
2019-04-23 14:44:37 -0500

    holding down button no longer allows cheating

--- rhythm.py
+++ rhythm.py
@@ -12,21 +12,46 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 done = False
 inner_r = 1
-color = (255,0,0)
+colors = [(255,0,0),(0,255,0)]
+color = 0
+button_pressed = False
+still_pressed = False
 while not done:
     for event in pygame.event.get():
+        #print(event)
         if event.type == pygame.QUIT:
             done = True
+        
 
-    pygame.draw.circle(screen,color,(WIDTH//2,HEIGHT//2),50)
+        # if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN and button_pressed:
+        #     still_pressed = True
+
+        # elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+        #     button_pressed = True
+
+        # elif event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
+        #     button_pressed = False
+        #     still_pressed = False
+
+    pygame.draw.circle(screen,colors[color],(WIDTH//2,HEIGHT//2),50)
     pygame.draw.circle(screen,(0,0,255),(WIDTH//2,HEIGHT//2),inner_r)
     
+    
+    
+    pressed = pygame.key.get_pressed()
+
+    if pressed[pygame.K_DOWN] and not button_pressed:
+        print('pressed')
+        button_pressed = True
+        if inner_r >=45: # closer to 50 is harder
+            color = (color + 1) % 2
+
+    if button_pressed and not pressed[pygame.K_DOWN]:
+        print('released')
+        button_pressed = False
+
     inner_r = (inner_r +1 )%50
     
-
-    pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP] and inner_r == 49:
-        color = (0,255,0)
     pygame.display.flip()
     screen.fill((0,0,0))
 
