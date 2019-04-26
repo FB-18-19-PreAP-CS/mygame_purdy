43e5696149c62920e5e3c28ad324b8769f70b876
Chad Purdy <chpurdy@gmail.com>
2019-04-25 20:40:45 -0500

    jumping works

--- walking_pgf.py
+++ walking_pgf.py
@@ -1,14 +1,23 @@
 from pygame_functions import *
 
+setAutoUpdate(False)
+
 screenSize(1000, 750)
-setBackgroundImage("./images/bg-icebergs-1.png")
+setBackgroundImage(["./images/bg-icebergs-1.png","./images/bg-icebergs-1.png"])
 
 penguin = makeSprite("./images/penguin_walk01.png")
 addSpriteImage(penguin,"./images/penguin_walk02.png")
 addSpriteImage(penguin,"./images/penguin_walk03.png")
 addSpriteImage(penguin,"./images/penguin_walk04.png")
+addSpriteImage(penguin,"./images/penguin_jump01.png")
+addSpriteImage(penguin,"./images/penguin_jump02.png")
+addSpriteImage(penguin,"./images/penguin_jump03.png")
 
 showSprite(penguin)
+
+instructions = makeLabel("Catch all the fish!",40,325,10)
+showLabel(instructions)
+
 
 ground = 575 # height of ground
 
@@ -18,19 +27,39 @@ yPos = 575
 xSpeed = 0
 ySpeed = 0
 
-jumping = False
+direction = 'right'
 
+jumping = False
+frame = 0
+nextFrame = clock()
 
 while True:
+
+    if clock() > nextFrame:
+        frame = (frame+1)%4
+        nextFrame += 80
+
     moveSprite(penguin,xPos,yPos,True) # True here makes it base position off of center of the sprite
 
     if keyPressed("left"):
+        direction = 'left'
+        if not jumping:
+            changeSpriteImage(penguin,frame)
+        transformSprite(penguin,0,1,hflip=True)
         xPos -= 3
 
     if keyPressed("right"):
+        direction = 'right'
+        if not jumping:
+            changeSpriteImage(penguin,frame)
         xPos += 3
 
     if keyPressed("up"):
+        if direction == 'right':
+            changeSpriteImage(penguin,6)
+        elif direction == 'left':
+            changeSpriteImage(penguin,6)
+            transformSprite(penguin,0,1,hflip=True)
         if not jumping:
             jumping = True
             ySpeed = 8
@@ -43,9 +72,13 @@ while True:
             ySpeed = 0
             yPos = ground
             jumping = False
+            changeSpriteImage(penguin,frame)
+            if direction == 'left':
+                transformSprite(penguin,0,1,hflip=True)
 
 
     
     tick(60)
+    updateDisplay()
 
 endWait()
